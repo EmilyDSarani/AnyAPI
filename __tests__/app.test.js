5e2420a0e2dd19e36f713a13b3b795e58d48289a
EmilyDSarani <emily.sarani@gmail.com>
2022-01-25 19:36:52 -0600

    get all songs is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Song = require('../lib/models/Song');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -19,5 +20,11 @@ describe('backend routes', () => {
     expect(res.body).toEqual({ id: expect.any(String), title: 'Sangria', artist: 'Blake Shelton', album: 'Bringing Back The Sunshine' });
   });
 
+  it('should get all songs', async () => {
+    await Song.insert({ title: 'Sangria', artist: 'Blake Shelton', album: 'Bringing Back The Sunshine' });
+    const res = await request(app).get('/api/v1/songs');
+
+    expect(res.body).toEqual([{  id: expect.any(String), title: 'Sangria', artist: 'Blake Shelton', album: 'Bringing Back The Sunshine' }]);
+  });
 
 });
